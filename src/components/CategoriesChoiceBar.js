3575a558012ba2183549be2b19f3e3ea26701ffa
Bartek Musiał <bartlomiej.musial@hotmail.com>
2022-03-02 08:35:46 +0100

    categories choice bar build

--- src/components/CategoriesChoiceBar.js
+++ src/components/CategoriesChoiceBar.js
@@ -7,7 +7,7 @@ const CategoriesChoiceBar = ({ additionalClass, categoryTabs }) => {
 
     return (
         <div className={['md:max-w-3xl flex justify-between '
-        + 'mx-auto px-2 sm:px-6 lg:px-8', ...additionalClass].join(' ')}
+        + 'mx-auto px-4 sm:px-6 lg:px-8', ...additionalClass].join(' ')}
         >
             <div className="sm:hidden w-full">
                 <label htmlFor="categoryTabs" className="sr-only">
@@ -17,8 +17,7 @@ const CategoriesChoiceBar = ({ additionalClass, categoryTabs }) => {
                 <select
                     id="tabs"
                     name="tabs"
-                    className="block w-full pl-3 pr-10 py-2 text-base border-gray-300
-                    focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm rounded-md"
+                    className="block w-full pl-3 pr-10 py-2 text-base sm:text-sm rounded-md"
                     defaultValue={categoryTabs.find((tab) => tab.current).name}
                 >
                     {categoryTabs.map((tab) => (
