34ad99bbed7e59f3c6568156690f9c3503d6dc00
Kinga <knapik.kinga@gmail.com>
2022-03-18 16:07:29 +0100

    fix layout

--- src/sections/RecipeCards.js
+++ src/sections/RecipeCards.js
@@ -3,7 +3,11 @@ import { Header } from 'flotiq-components-react';
 import RecipeCard from '../components/RecipeCard';
 
 const RecipeCards = ({ recipes, headerText, compact = false }) => (
-    <div className="max-w-7xl mt-6 mb-6 mx-auto px-4 py-4 sm:px-6 lg:px-8 xl:px-0">
+    <div
+        className={compact
+            ? 'max-w-7xl mt-6 mb-6 mx-auto px-4 py-4 sm:px-6 lg:px-8'
+            : 'max-w-7xl mt-6 mb-6 mx-auto px-4 py-4 sm:px-6 lg:px-8 xl:px-0'}
+    >
         {headerText && <Header additionalClasses={['basis-1 mb-2 !text-3xl !font-medium']} text={headerText} />}
         <div className={compact ? 'grid md:grid-cols-2 lg:grid-cols-3 gap-6' : 'flex flex-col'}>
             {recipes.map((recipe) => (
